4e986ca405d973712ff72d83698f4b4e1fe7c78f
Tom <tom_clay777@live.com>
2020-10-20 14:29:04 -0400

    Implementation of hk_Limited_Ball_Socket_Constraint::get_constraint_type() Implementation of hk_Limited_Ball_Socket_Constraint::get_constraint_dof()

--- havana/havok/hk_physics/constraint/limited_ball_socket/limited_ball_socket_constraint.h
+++ havana/havok/hk_physics/constraint/limited_ball_socket/limited_ball_socket_constraint.h
@@ -30,6 +30,22 @@ class hk_Limited_Ball_Socket_Constraint : public hk_Constraint
 
 		void init_constraint( const void* bp );
 
+		virtual const char* get_constraint_type()
+		{
+			return "limited_ball_socket";
+		}
+
+		virtual int get_constraint_dof()
+		{
+			/*
+			* 		push    ebp
+			* 		mov     eax, 0x3
+			* 		mov     ebp, esp
+			* 		pop     ebp
+			* 		ret
+			*/
+			return 3;
+		}
 	protected:
 
 		void init_ball_socket_constraint( const hk_Limited_Ball_Socket_BP *bp );
