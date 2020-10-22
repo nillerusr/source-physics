7ac024a8f9a24893defd36b8c9343176ea59f331
Tom <tom_clay777@live.com>
2020-10-21 21:00:04 -0400

    Incomplete implementation of hk_Fixed_Constraint::get_transform

--- havana/havok/hk_physics/constraint/fixed/fixed_constraint.h
+++ havana/havok/hk_physics/constraint/fixed/fixed_constraint.h
@@ -40,6 +40,12 @@ class hk_Fixed_Constraint : public hk_Constraint
 			return "fixed";
 		}
 
+		inline hk_Transform get_transform(int x) const
+		{
+			// todo(crack): this is probably wrong... fix me
+			HK_ASSERT(0 && "Incomplete implementation");
+			return m_transform_os_ks;
+		}
 	protected:
 
 		void init_fixed_constraint( const hk_Fixed_BP *bp );
