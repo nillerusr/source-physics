4c3698094969a026f5c27c5b69bbce0cdfb7e33d
hymei <824395314@qq.com>
2022-03-02 23:52:49 +0800

    arm64 fix

--- ivp_utility/ivu_memory.cxx
+++ ivp_utility/ivu_memory.cxx
@@ -132,7 +132,7 @@ void *ivp_malloc_aligned(int size, int alignment){
     IVP_Aligned_Memory *data = (IVP_Aligned_Memory*)p_malloc( (unsigned int) size);
     data->magic_number = IVP_MEMORY_MAGIC;
     
-    void *ret = (void *)((((long)data) + alignment + sizeof(IVP_Aligned_Memory) - 1) & (-alignment));
+    void *ret = (void *)((((intp)data) + alignment + sizeof(IVP_Aligned_Memory) - 1) & (-alignment));
     ((void **)ret)[-1] = (void *)data;
     return ret;
 #endif    
